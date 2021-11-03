c12f04b59134cd0032e19db05a0e650090500214
RafhaelKelve <82288130+RafhaelKelve@users.noreply.github.com>
2021-11-03 10:36:35 -0300

    foi criada uma pasta _MODELO e configurei o composer, HomeController, LoginController e o Models

--- devsbook/src/controllers/HomeController.php
+++ devsbook/src/controllers/HomeController.php
@@ -5,6 +5,12 @@ use \core\Controller;
 
 class HomeController extends Controller {
 
+    private $loggedUser;
+
+    public function __construct()  {
+        $this->redirect('/login');
+    }
+
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
     }
