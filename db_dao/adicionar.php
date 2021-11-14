3c12b3884cb59932bf8f1026bf71f9afdff6a672
RafhaelKelve <82288130+RafhaelKelve@users.noreply.github.com>
2021-11-14 11:40:08 -0300

    Criei um arquivo adicionar.php com o código do formulário

---
+++ db_dao/adicionar.php
@@ -0,0 +1,20 @@
+<h1><b><u>Adicionar um Novo Usuário</u></b></h1>
+<hr>
+
+<a href="index.php"><button style="background-color: blue; color: #ffffff;">VOLTAR</button></a>
+
+<br><br>
+
+<form method="POST" action="adicionar_action.php">
+    <label>
+        NOME: <br>
+        <input type="text" name="name">
+    </label> <br><br>
+
+    <label>
+        EMAIL: <br>
+        <input type="email" name="email">
+    </label> <br><br>
+
+    <input type="submit" value="SALVAR">
+</form>
