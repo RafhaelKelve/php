5c11c84238c45d050b5e662e3f990b691d733ddc
RafhaelKelve <82288130+RafhaelKelve@users.noreply.github.com>
2021-11-19 18:49:39 -0300

    Criei umas subpasta assets com umas pastas css, js, images e inserir códigos

---
+++ classificados/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classificados</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand">Classificados</a>
+            </div>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+           <li><a href="">Cadastre-se</a></li> 
+           <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h2>Nós temos hoje 999 anúncios publicados</h2>
+            <p>E mais de 999 usuários cadastrados.</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+                <h4>Pesquisa Avançada</h4>
+            </div>
+            <div class="col-sm-3">
+                <h4>últimos Anúncios</h4>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
