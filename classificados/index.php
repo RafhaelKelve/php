4970dcf299cfa04a4443d2a91e93eaf41822c12e
RafhaelKelve <82288130+RafhaelKelve@users.noreply.github.com>
2021-11-19 19:14:56 -0300

    criei uma pasta pages e com os arquivos header.php e footer.php

--- classificados/index.php
+++ classificados/index.php
@@ -1,32 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classificados</title>
+<?php require 'pages/header.php'; ?>
 
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="assets/css/style.css" />
-
-    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/script.js"></script>
-
-</head>
-<body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a href="./" class="navbar-brand">Classificados</a>
-            </div>
-        </div>
-
-        <ul class="nav navbar-nav navbar-right">
-           <li><a href="">Cadastre-se</a></li> 
-           <li><a href="">Login</a></li>
-        </ul>
-    </nav>
     <div class="container-fluid">
         <div class="jumbotron">
             <h2>Nós temos hoje 999 anúncios publicados</h2>
@@ -43,5 +16,4 @@
         </div>
     </div>
     
-</body>
-</html>
+<?php require 'pages/footer.php'; ?>
